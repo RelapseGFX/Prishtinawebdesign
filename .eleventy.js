a727de067d3880babfb462da8e3336f2b2065f42
RelapseGFX <albion.metollii@gmail.com>
2025-02-22 21:01:59 +0100

    added matrix

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/styles.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/darkmode.js');
+    eleventyConfig.addPassthroughCopy('./src/matrixrentals.html');
 
     return {
         dir: {
