717a1bbb717073bc12210ce291bac347d52d0aeb
RelapseGFX <albion.metollii@gmail.com>
2025-03-12 22:40:24 +0100

    wtf

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/styles.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/darkmode.js');
-    eleventyConfig.addPassthroughCopy('./src/matrixrentals.html');
 
     return {
         dir: {
