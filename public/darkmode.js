d40e0f593548e765c550ffccc444534d75b31419
RelapseGFX <albion.metollii@gmail.com>
2025-03-12 23:32:04 +0100

    updated

--- public/darkmode.js
+++ public/darkmode.js
@@ -1,30 +1,27 @@
-let darkMode = localStorage.getItem('darkMode');
+let darkMode = localStorage.getItem('darkMode')
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 const iconswitch = document.querySelector('#iconswitch');
+const illustrations = document.querySelectorAll('.illustration');
 
 const enableDarkMode = () => {
     document.body.classList.add("darkmode");
-    localStorage.setItem("darkMode", "enabled");
+    localStorage.setItem("darkMode", "enabled")
+    iconswitch.src = "/assets/images/sun.svg"; 
 };
 
 const disableDarkMode = () => {
     document.body.classList.remove("darkmode");
     localStorage.setItem("darkMode", null);
+    iconswitch.src = "/assets/images/moon.svg";
 };
 
-const updateIcon = () => {
-    if (darkMode === 'enabled') {
-        iconswitch.src = "assets/images/sun.svg";
-    } else {
-        iconswitch.src = "assets/images/moon.svg";
-    }
+const getBasePath = () => {
+  return window.location.pathname.includes("index.html") ? "/assets/images/sun.sv" : "/assets/images/moon.svg";
 };
 
-// Initialize dark mode state
 if (darkMode === "enabled") {
     enableDarkMode();
 }
-updateIcon(); // Update icon based on initial state
 
 darkModeToggle.addEventListener("click", () => {
     darkMode = localStorage.getItem("darkMode");
@@ -33,14 +30,21 @@ darkModeToggle.addEventListener("click", () => {
     } else {
         disableDarkMode();
     }
-    updateIcon(); // Update icon after toggling
+    if(darkMode !== "enabled") {
+        iconswitch.src = "/assets/images/sun.svg"
+      }else{
+        iconswitch.src = "/assets/images/moon.svg"
+      };
 });
+const body = document.querySelector('body');
+    
+document.addEventListener('scroll' , () => {
+  const header = document.querySelector('header');
 
-document.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    if (window.scrollY > 0) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
+  if (window.scrollY > 0) {
+    header.classList.add('scrolled');
+  } else {
+    header.classList.remove('scrolled');
+  }
+
 });
