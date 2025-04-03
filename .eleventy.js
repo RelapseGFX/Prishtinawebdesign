59c27f555a6d9d54e313b8e8d4e62c15efd05599
RelapseGFX <albion.metollii@gmail.com>
2025-04-03 15:15:13 +0200

    css

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./src/style.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/darkmode.js');
-    eleventyConfig.addPassthroughCopy('./src/work.css');
 
     return {
         dir: {
